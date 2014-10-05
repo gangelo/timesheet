ebb3526276651840a92812ffc610ac428b5fadae
gangelo <web.gma@gmail.com>
2014-10-04 22:24:09 -0400

    Clean up code to determine start and end dates for the timesheets

--- Rakefile
+++ Rakefile
@@ -4,8 +4,8 @@ begin
   require 'rspec/core/rake_task'
   RSpec::Core::RakeTask.new(:spec)
 rescue LoadError => e
-  task "spec" do
-    puts "RSpec not loaded - make sure it's installed and you're using bundle exec"
+  task 'spec' do
+    puts 'RSpec not loaded - make sure it's installed and you're using bundle exec'
     exit 1
   end
 end
--- timesheet.rb
+++ timesheet.rb
@@ -3,7 +3,7 @@
 require 'date'
 
 module Timesheet
-
+  # The timesheet class. Generates my timesheet file names.
   class Timesheet
     attr_reader :date, :week_number
 
@@ -19,65 +19,41 @@ module Timesheet
     end
 
     def show
-      puts 'The date entered is invalid' and return unless valid?
+      puts 'The date entered is invalid' && return unless valid?
       puts rcmt_filename
       puts sandata_filename
     end
 
-    def self.get_date
+    def self.gets_date
       date = gets.chomp
       return date unless date.empty?
-      return DateTime.now
+      DateTime.now
     end
 
-    def self.get_week_number
+    def self.gets_week_number
       week_number = gets.chomp
       return week_number unless week_number.empty?
-      return week_number.to_i
+      week_number.to_i
     end
 
-    protected 
+    protected
 
     def rcmt_filename
-      start_date = nil
-      end_date = nil
+      start_date = find_start_date_for(@date, :sunday?)
+      end_date = find_end_date_for(@date, :saturday?)
 
-      start_date_for(@date) do |date| 
-        if date.sunday?
-          start_date = date
-          break
-        end 
-      end
-
-      end_date_for(@date) do |date| 
-        if date.saturday?
-          end_date = date
-          break
-        end 
-      end
-
-      "#{@week_number} - RCMT Timesheet - #{format_date(start_date)} thru #{format_date(end_date)}"
+      "#{@week_number} - RCMT Timesheet - " \
+        "#{format_date(start_date)} thru " \
+          "#{format_date(end_date)}"
     end
 
     def sandata_filename
-      start_date = nil
-      end_date = nil
+      start_date = find_start_date_for(@date, :saturday?)
+      end_date = find_end_date_for(@date, :friday?)
 
-      start_date_for(@date) do |date| 
-        if date.saturday?
-          start_date = date
-          break
-        end 
-      end
-
-      end_date_for(@date) do |date| 
-        if date.friday?
-          end_date = date
-          break
-        end 
-      end
-
-      "#{@week_number} - Sandata Timesheet - #{format_date(start_date)} thru #{format_date(end_date)}"
+      "#{@week_number} - Sandata Timesheet - " \
+        "#{format_date(start_date)} thru " \
+          "#{format_date(end_date)}"
     end
 
     def start_date_for(date)
@@ -98,15 +74,32 @@ module Timesheet
       end
     end
 
+     def find_start_date_for(start_date, eval)
+      start_date_for(start_date) do |date|
+        if date.instance_eval eval.to_s
+          return date
+        end
+      end
+    end
+
+    def find_end_date_for(end_date, eval)
+      end_date_for(end_date) do |date|
+        if date.instance_eval eval.to_s
+          return date
+        end
+      end
+    end
+
     def format_date(date)
       date.strftime('%a %F') if date.respond_to? :strftime
     end
-    
+
     def scrub_date
       begin
         @date = DateTime.parse(@date) unless @date.is_a? DateTime
         @valid = true
-      rescue
+      rescue ArgumentError => e
+        puts "Error parsing input date: #{e.message}"
         @date = DateTime.now
         @valid = false
       end
@@ -114,11 +107,12 @@ module Timesheet
   end
 end
 
-print 'Enter a date (yyyy-mm-dd) that falls within the week starting/ending dates (blank for current date): '
-date = Timesheet::Timesheet.get_date
+print 'Enter a date (yyyy-mm-dd) ' \
+  'that falls within the week starting/ending dates (blank for current date): '
+date = Timesheet::Timesheet.gets_date
 
 print 'Enter the week number for this timesheet period: '
-week_number = Timesheet::Timesheet.get_week_number
+week_number = Timesheet::Timesheet.gets_week_number
 
 timesheet = Timesheet::Timesheet.new date, week_number
 timesheet.show
